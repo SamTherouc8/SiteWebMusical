87cf48bb0a99664431cfd8876644747123e4eecb
SamTheRouc8 <SamTheRouc8@gmail.com>
2026-03-05 21:50:18 -0500

    5 mars 2025: initiation au javascript, re fonctionnement des positions, beaucoup de difficultés, début de l'exercice 1 (5h)

---
+++ Instruments/Saxophone/Exercises/Exercise 1/Exercise1.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+document.getElementById(`start-button`).onclick = function(){
+    document.getElementById(`main-container`).style.display = `inline`
+    document.getElementById(`saxophone-container`).style.display = `inline`
+    document.getElementById(`start-button-div`).style.display = `none`
+
+    let status = true;
+    while(status === true){
+        
+    }
+}
