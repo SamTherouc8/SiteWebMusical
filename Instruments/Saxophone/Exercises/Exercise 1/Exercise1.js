948daa73fd8a2ab69f704d5e2c9abc265904718b
SamTheRouc8 <SamTheRouc8@gmail.com>
2026-03-06 18:51:33 -0500

    test2

--- Instruments/Saxophone/Exercises/Exercise 1/Exercise1.js
+++ Instruments/Saxophone/Exercises/Exercise 1/Exercise1.js
@@ -6,7 +6,9 @@
 document.getElementById(`start-button`).onclick = function(){
     document.getElementById(`main-container`).style.display = `inline`
     document.getElementById(`saxophone-container`).style.display = `inline`
+    document.getElementById(`testcontainer`).style.display = `inline`
     document.getElementById(`start-button-div`).style.display = `none`
+
 
     //let status = true;
     //while(status === true){
