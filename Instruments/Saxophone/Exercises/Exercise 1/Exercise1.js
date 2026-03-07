915401c29114b1f382fd2a92a6203744a5c950b5
SamTheRouc8 <SamTheRouc8@gmail.com>
2026-03-06 21:51:24 -0500

    6 mars 2026: hitbox des boutons, fonctionnement avec d'autres appareils, host le site

--- Instruments/Saxophone/Exercises/Exercise 1/Exercise1.js
+++ Instruments/Saxophone/Exercises/Exercise 1/Exercise1.js
@@ -9,9 +9,17 @@ document.getElementById(`start-button`).onclick = function(){
     document.getElementById(`testcontainer`).style.display = `inline`
     document.getElementById(`start-button-div`).style.display = `none`
 
+    document.getElementById(`key1-input`).onclick = console.log(`yoo`)
+
+
 
     //let status = true;
     //while(status === true){
     //    
     //}
+}
+
+function check() {
+    document.getElementById("myCheck").checked = true;
+    
 }
