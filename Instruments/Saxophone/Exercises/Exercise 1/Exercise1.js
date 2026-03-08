3d50c5799c8e0bdbe5ec3349e00de602256b3f10
SamTheRouc8 <SamTheRouc8@gmail.com>
2026-03-07 21:26:57 -0500

    7 mars 2026: ajout du fonctionnement du clic des boutons, ajout du reset des boutons, ajout du timer, ajout du fonctionnement des images (6h)

--- Instruments/Saxophone/Exercises/Exercise 1/Exercise1.js
+++ Instruments/Saxophone/Exercises/Exercise 1/Exercise1.js
@@ -2,14 +2,16 @@
 
 
 
+function startProgram(){
+    document.getElementById(`main-container`).style.display = `inline`;
+    document.getElementById(`saxophone-container`).style.display = `inline`;
+    document.getElementById(`testcontainer`).style.display = `inline`;
+    document.getElementById(`my-timer`).style.display = `block`;
+    document.getElementById(`start-button-div`).style.display = `none`;
 
-document.getElementById(`start-button`).onclick = function(){
-    document.getElementById(`main-container`).style.display = `inline`
-    document.getElementById(`saxophone-container`).style.display = `inline`
-    document.getElementById(`testcontainer`).style.display = `inline`
-    document.getElementById(`start-button-div`).style.display = `none`
+    startTimer();
 
-    document.getElementById(`key1-input`).onclick = console.log(`yoo`)
+
 
 
 
@@ -19,7 +21,86 @@ document.getElementById(`start-button`).onclick = function(){
     //}
 }
 
-function check() {
-    document.getElementById("myCheck").checked = true;
+
+function nameFunction(id) {
+ 
+   //Get the checkbox
+  var checkBox = document.getElementById(`${id}-input`);
+
+   //If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    console.log(`${id} was checked`);
+  } else {
+    console.log(`${id} was unchecked`);
+  }
+}
+
+
+function note(){
+    //...
+}
+
+function changeImage(){
+    //...
+}
+
+function reset(){
+    const elements = document.getElementsByClassName("input-key");
+    for (let element of elements) {
+        element.checked = false;
+    }
+    console.log("Checkboxes resetted.")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const display = document.getElementById("my-timer");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function startTimer(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(updateTimer, 10);
+        isRunning = true;
+    }
+}
+
+
+function resetTimer(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;    
+    display.textContent = "00:00:00";
+}
+
+function updateTimer(){
     
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    //milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}`;
 }
